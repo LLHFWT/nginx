751de076091ae1e616b50c7f3fc726c614a6b02a
Fu Weiting <fwtllh@126.com>
2022-06-16 15:05:50 +0800

    Update ngx_shmtx.h

--- src/core/ngx_shmtx.h
+++ src/core/ngx_shmtx.h
@@ -15,6 +15,7 @@
 
 typedef struct {
     ngx_atomic_t   lock;
+    // 如果系统支持信号量则使用信号量唤醒等待锁的进程
 #if (NGX_HAVE_POSIX_SEM)
     ngx_atomic_t   wait;
 #endif
@@ -22,6 +23,7 @@ typedef struct {
 
 
 typedef struct {
+    // 如果系统支持原子操作则使用原子操作实现锁
 #if (NGX_HAVE_ATOMIC_OPS)
     ngx_atomic_t  *lock;
 #if (NGX_HAVE_POSIX_SEM)
@@ -29,19 +31,25 @@ typedef struct {
     ngx_uint_t     semaphore;
     sem_t          sem;
 #endif
+    // 如果系统不支持原子操作则使用文件锁
 #else
     ngx_fd_t       fd;
     u_char        *name;
 #endif
-    ngx_uint_t     spin;
+    ngx_uint_t     spin; // 控制自旋次数
 } ngx_shmtx_t;
 
 
+// 创建共享内存锁，mtx是进程创建的、用于存储锁的变量，addr是共享内存块中用于标识锁的变量
 ngx_int_t ngx_shmtx_create(ngx_shmtx_t *mtx, ngx_shmtx_sh_t *addr,
     u_char *name);
+// 销毁共享内存锁
 void ngx_shmtx_destroy(ngx_shmtx_t *mtx);
+// 尝试加锁，失败直接返回
 ngx_uint_t ngx_shmtx_trylock(ngx_shmtx_t *mtx);
+// 一直等待加锁直到成功
 void ngx_shmtx_lock(ngx_shmtx_t *mtx);
+// 释放锁
 void ngx_shmtx_unlock(ngx_shmtx_t *mtx);
 ngx_uint_t ngx_shmtx_force_unlock(ngx_shmtx_t *mtx, ngx_pid_t pid);
 
