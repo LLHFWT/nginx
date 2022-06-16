f126c9cd203e77ce241c6cf05cc865d483690300
Fu Weiting <fwtllh@126.com>
2022-06-16 14:55:45 +0800

    Update ngx_shmem.h

--- src/os/unix/ngx_shmem.h
+++ src/os/unix/ngx_shmem.h
@@ -12,17 +12,18 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+// 共享内存块结构描述
 typedef struct {
-    u_char      *addr;
-    size_t       size;
-    ngx_str_t    name;
+    u_char      *addr; // 共享内存地址
+    size_t       size; // 共享内存大小
+    ngx_str_t    name; // 名字
     ngx_log_t   *log;
     ngx_uint_t   exists;   /* unsigned  exists:1;  */
 } ngx_shm_t;
 
-
+// 分配一块共享内存，根据系统选择mmap、shmget来实现
 ngx_int_t ngx_shm_alloc(ngx_shm_t *shm);
+// 释放共享内存
 void ngx_shm_free(ngx_shm_t *shm);
 
 
