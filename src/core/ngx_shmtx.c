ca4bd4648de31102f46cd9469d5557b0ff5e1d94
Fu Weiting <fwtllh@126.com>
2022-06-16 15:24:34 +0800

    Update ngx_shmtx.c

--- src/core/ngx_shmtx.c
+++ src/core/ngx_shmtx.c
@@ -11,19 +11,21 @@
 
 #if (NGX_HAVE_ATOMIC_OPS)
 
-
+// 进程等待被唤醒
 static void ngx_shmtx_wakeup(ngx_shmtx_t *mtx);
 
 
 ngx_int_t
 ngx_shmtx_create(ngx_shmtx_t *mtx, ngx_shmtx_sh_t *addr, u_char *name)
 {
+    // 将共享内存中的锁地址赋值到此处
     mtx->lock = &addr->lock;
 
     if (mtx->spin == (ngx_uint_t) -1) {
         return NGX_OK;
     }
 
+    // 设置自旋
     mtx->spin = 2048;
 
 #if (NGX_HAVE_POSIX_SEM)
@@ -62,6 +64,7 @@ ngx_shmtx_destroy(ngx_shmtx_t *mtx)
 ngx_uint_t
 ngx_shmtx_trylock(ngx_shmtx_t *mtx)
 {
+    // 如果当前没获得锁并原子加锁成功，则获取锁成功，
     return (*mtx->lock == 0 && ngx_atomic_cmp_set(mtx->lock, 0, ngx_pid));
 }
 
@@ -75,12 +78,14 @@ ngx_shmtx_lock(ngx_shmtx_t *mtx)
 
     for ( ;; ) {
 
+        // 尝试原子获取锁
         if (*mtx->lock == 0 && ngx_atomic_cmp_set(mtx->lock, 0, ngx_pid)) {
             return;
         }
 
-        if (ngx_ncpu > 1) {
+        if (ngx_ncpu > 1) { // 如果有多个CPU
 
+            // 根据自旋设置，让出cpu，让出cpu次数逐渐增加
             for (n = 1; n < mtx->spin; n <<= 1) {
 
                 for (i = 0; i < n; i++) {
@@ -97,9 +102,12 @@ ngx_shmtx_lock(ngx_shmtx_t *mtx)
 
 #if (NGX_HAVE_POSIX_SEM)
 
+        // 原子获取锁失败，使用信号量等待唤醒
         if (mtx->semaphore) {
+            //等待进程数加一
             (void) ngx_atomic_fetch_add(mtx->wait, 1);
 
+            // 再尝试一次
             if (*mtx->lock == 0 && ngx_atomic_cmp_set(mtx->lock, 0, ngx_pid)) {
                 (void) ngx_atomic_fetch_add(mtx->wait, -1);
                 return;
@@ -108,6 +116,7 @@ ngx_shmtx_lock(ngx_shmtx_t *mtx)
             ngx_log_debug1(NGX_LOG_DEBUG_CORE, ngx_cycle->log, 0,
                            "shmtx wait %uA", *mtx->wait);
 
+            // 等待唤醒
             while (sem_wait(&mtx->sem) == -1) {
                 ngx_err_t  err;
 
@@ -128,6 +137,7 @@ ngx_shmtx_lock(ngx_shmtx_t *mtx)
 
 #endif
 
+        // 让出当前进程调度周期，然后再次进入循环
         ngx_sched_yield();
     }
 }
@@ -140,6 +150,7 @@ ngx_shmtx_unlock(ngx_shmtx_t *mtx)
         ngx_log_debug0(NGX_LOG_DEBUG_CORE, ngx_cycle->log, 0, "shmtx unlock");
     }
 
+    // 原子变量置零，释放成功时唤醒其他等待进程
     if (ngx_atomic_cmp_set(mtx->lock, ngx_pid, 0)) {
         ngx_shmtx_wakeup(mtx);
     }
@@ -152,6 +163,7 @@ ngx_shmtx_force_unlock(ngx_shmtx_t *mtx, ngx_pid_t pid)
     ngx_log_debug0(NGX_LOG_DEBUG_CORE, ngx_cycle->log, 0,
                    "shmtx forced unlock");
 
+    // 强制释放指定pid进程的锁
     if (ngx_atomic_cmp_set(mtx->lock, pid, 0)) {
         ngx_shmtx_wakeup(mtx);
         return 1;
@@ -160,17 +172,18 @@ ngx_shmtx_force_unlock(ngx_shmtx_t *mtx, ngx_pid_t pid)
     return 0;
 }
 
-
 static void
 ngx_shmtx_wakeup(ngx_shmtx_t *mtx)
 {
 #if (NGX_HAVE_POSIX_SEM)
     ngx_atomic_uint_t  wait;
 
+    // 检查是否有进程在等待
     if (!mtx->semaphore) {
         return;
     }
 
+    // 等待进程数减一
     for ( ;; ) {
 
         wait = *mtx->wait;
@@ -187,6 +200,7 @@ ngx_shmtx_wakeup(ngx_shmtx_t *mtx)
     ngx_log_debug1(NGX_LOG_DEBUG_CORE, ngx_cycle->log, 0,
                    "shmtx wake %uA", wait);
 
+    // 通知等待进程
     if (sem_post(&mtx->sem) == -1) {
         ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, ngx_errno,
                       "sem_post() failed while wake shmtx");
