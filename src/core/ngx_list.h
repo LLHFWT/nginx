6030c68798aa4b1b347cfd519a8d4950f29747f2
Fu Weiting <fwtllh@126.com>
2022-06-20 15:32:37 +0800

    Update ngx_list.h

--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -12,27 +12,31 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+// nginx list实现，结构相当于链接起来的数组，每一个节点可看成一块大小相等的内存，可以存多个元素
+// 链表元素只能增加
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
+// list的节点，每个节点可以存多个元素，最大是下面的nalloc
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
-    ngx_list_part_t  *next;
+    void             *elts; // 元素存储地址
+    ngx_uint_t        nelts; // 当前已存的元素数
+    ngx_list_part_t  *next; // 下一个节点 
 };
 
 
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
-    size_t            size;
-    ngx_uint_t        nalloc;
+    ngx_list_part_t  *last; // 最后一个链表节点
+    ngx_list_part_t   part; // 第一个链表节点
+    size_t            size; // 每个元素大小
+    ngx_uint_t        nalloc; // 每个节点可存的元素数
     ngx_pool_t       *pool;
 } ngx_list_t;
 
 
+// 创建一个list，元素大小是size，每个节点可以存n个元素
 ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
 
+// 初始化
 static ngx_inline ngx_int_t
 ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
@@ -76,7 +80,7 @@ ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
  *  }
  */
 
-
+// 增加元素返回其内存地址
 void *ngx_list_push(ngx_list_t *list);
 
 
