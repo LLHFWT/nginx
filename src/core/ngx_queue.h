42b5faf1acbdcb22b10ca7d8272dc58285a0fbd5
Fu Weiting <fwtllh@126.com>
2022-06-20 16:31:30 +0800

    Update ngx_queue.h

--- src/core/ngx_queue.h
+++ src/core/ngx_queue.h
@@ -20,26 +20,29 @@ struct ngx_queue_s {
     ngx_queue_t  *next;
 };
 
-
+// 初始化，prev和next都指向自己
 #define ngx_queue_init(q)                                                     \
     (q)->prev = q;                                                            \
     (q)->next = q
 
 
+// 判空，prev指向自己
 #define ngx_queue_empty(h)                                                    \
     (h == (h)->prev)
 
 
+// 插入头部
 #define ngx_queue_insert_head(h, x)                                           \
     (x)->next = (h)->next;                                                    \
     (x)->next->prev = x;                                                      \
     (x)->prev = h;                                                            \
     (h)->next = x
 
-
+// 插入到第一个参数节点之后
 #define ngx_queue_insert_after   ngx_queue_insert_head
 
 
+// 插入到尾部
 #define ngx_queue_insert_tail(h, x)                                           \
     (x)->prev = (h)->prev;                                                    \
     (x)->prev->next = x;                                                      \
@@ -47,10 +50,12 @@ struct ngx_queue_s {
     (h)->prev = x
 
 
+// 获取第一个节点
 #define ngx_queue_head(h)                                                     \
     (h)->next
 
 
+// 获取最后一个节点
 #define ngx_queue_last(h)                                                     \
     (h)->prev
 
@@ -77,6 +82,7 @@ struct ngx_queue_s {
 
 #else
 
+// 移除节点
 #define ngx_queue_remove(x)                                                   \
     (x)->next->prev = (x)->prev;                                              \
     (x)->prev->next = (x)->next
@@ -104,7 +110,9 @@ struct ngx_queue_s {
     (type *) ((u_char *) q - offsetof(type, link))
 
 
+// 查找中间节点
 ngx_queue_t *ngx_queue_middle(ngx_queue_t *queue);
+// 插入排序
 void ngx_queue_sort(ngx_queue_t *queue,
     ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *));
 
